5a084212bac0bbb20f0ce6b1e3288789b8b16d7c
naoki709mm <naoki709mm@gmail.com>
2018-06-14 13:56:36 +0900

    prog3-1 OK

--- prog3-1.c
+++ prog3-1.c
@@ -1,15 +1,19 @@
+/*** 氏名(Name) 奥屋 直己
+ *** 課題 3 問題 1 : ヘロンの公式で三角形の面積を求める関数を作成し、面積を求めるプログラム
+ *** 提出日: 6月14日
+ ***/
 #include<stdio.h>
 #include<math.h>
 
-double Heron(double a,double b,double c){
+double Heron(double a,double b,double c){ //ヘロンの公式の関数
   double S;
   double s;
   
-  if(a<=0 || b<=0 || c <=0)
+  if(a<=0 || b<=0 || c<=0) //三角形の各辺が0以下の時、-1を返す
     return -1;
-  s=(a+b+c)/2.0;
-  S=sqrt(s*(s-a)*(s-b)*(s-c));
   
+  s=(a+b+c)/2.0; //ヘロンの公式
+  S=sqrt(s*(s-a)*(s-b)*(s-c));
   return S;
 }
 
@@ -17,12 +21,12 @@ int main(void){
   double a,b,c;
   double S;
   
-  printf("a = ");scanf("%lf",&a);
+  printf("a = ");scanf("%lf",&a); //三角形の各辺をそれぞれa,b,cに代入
   printf("b = ");scanf("%lf",&b);
   printf("c = ");scanf("%lf",&c);
-
-  S=Heron(a,b,c);
-  if(S==-1){
+  
+  S=Heron(a,b,c); //ヘロンの公式の関数へ各辺の値を渡す
+  if(S==-1){ //関数が-1を返した時、エラーを表示
     printf("\n");
     printf("エラー\n");
   }
@@ -30,6 +34,6 @@ int main(void){
     printf("\n");
     printf("三角形の面積Sは%5.1fです。\n",S);
   }
-    
+  
   return 0;
 }
